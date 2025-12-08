6742843983386124ebd6ada2d7709b793a20b7df
larchanka <larchanka@me.com>
2025-12-08 22:02:20 +0100

    chore: add local domain

--- start-services.sh
+++ start-services.sh
@@ -5,16 +5,16 @@ echo "🚀 Запускаю сервисы Docker Compose..."
 docker-compose up -d
 
 echo "⏳ Инициализирую контейнеры..."
-sleep 5
-
-# Фронтенд
-FRONTEND_CONTAINER=$(docker ps -q -f name=frontend-1)
-echo "🧠 Устанавливаю зависимости и запускаю Фронтенд..."
-docker exec -d $FRONTEND_CONTAINER bash -c "npm install && npm run dev"
+sleep 15
 
 # Бэкенд (API)
 API_CONTAINER=$(docker ps -q -f name=api-1)
 echo "🐍 Устанавливаю зависимости и запускаю Бэкенд..."
 docker exec -d $API_CONTAINER bash -c "pip install --no-cache-dir --upgrade -r requirements.txt && fastapi dev app/main.py --host 0.0.0"
+
+# Фронтенд
+FRONTEND_CONTAINER=$(docker ps -q -f name=frontend-1)
+echo "🧠 Устанавливаю зависимости и запускаю Фронтенд..."
+docker exec -d $FRONTEND_CONTAINER bash -c "cd /home/app && npm install && npm run dev --host 0.0.0"
 
 echo "✅ Фронтенд и API-Бэкенд запущены!"
