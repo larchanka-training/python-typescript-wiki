38fffd8e39e03528b741f0646f3841d2c500fe1d
larchanka <larchanka@me.com>
2026-02-02 20:47:13 +0100

    fix: migrations

--- start-services.sh
+++ start-services.sh
@@ -7,10 +7,10 @@ docker-compose up -d
 echo "⏳ Инициализирую контейнеры..."
 sleep 15
 
-# Бэкенд (API)
-# API_CONTAINER=$(docker ps -q -f name=api-1)
-# echo "🐍 Устанавливаю зависимости и запускаю Бэкенд..."
-# docker exec -d $API_CONTAINER bash -c "pip install --no-cache-dir --upgrade -r requirements.txt && fastapi dev app/main.py --host 0.0.0.0"
+Бэкенд (API)
+API_CONTAINER=$(docker ps -q -f name=api-1)
+echo "🐍 Устанавливаю зависимости и запускаю Бэкенд..."
+docker exec -d $API_CONTAINER bash -c "pip install --no-cache-dir --upgrade -r requirements.txt && fastapi dev app/main.py --host 0.0.0.0"
 
 # Фронтенд
 FRONTEND_CONTAINER=$(docker ps -q -f name=frontend-1)
